44b5a801f6270ab17acd306a29136a5c2ba18264
Diogo Ide <diogoide@gmail.com>
2018-03-17 23:02:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -60,7 +60,7 @@ def winner(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    
+
   if won?(board) && position_1 == "X"
   return "X"
   elsif won?(board) && position_1 == "O"
